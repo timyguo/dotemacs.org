64bedc7fbbf7ec33ae18bb0f2e069370cf3d1597
Tony Day <zygomega@gmail.com>
2012-10-20 18:29:33 +1100

    a rough working dotemacs.org

--- init.el
+++ init.el
@@ -1,21 +1,14 @@
-;;; init.el --- Where all the magic begins
-;;
-;; Part of the Emacs Starter Kit
-;;
-;; This is the first thing to get loaded.
-;;
+
+;; see org-init.org for documentation
 
 ;; remember this directory
-(setq starter-kit-dir
+(setq working-dir
       (file-name-directory (or load-file-name (buffer-file-name))))
 
-(let ((elisp-dir (expand-file-name "src" starter-kit-dir)))
+;; put the org dir on the path if it exists
+(let ((org-dir (expand-file-name "src/org-mode/lisp" working-dir)))
     ;; add the src directory to the load path
-    (add-to-list 'load-path elisp-dir)
-    ;; load specific files
-    (when (file-exists-p elisp-dir)
-      (let ((default-directory elisp-dir))
-        (normal-top-level-add-subdirs-to-load-path))))
+    (add-to-list 'load-path org-dir))
  
-;; load up the starter kit
-(org-babel-load-file (expand-file-name "starter-kit.org" starter-kit-dir))
+;; load up the org-init file
+(org-babel-load-file (expand-file-name "dotemacs.org" working-dir))
