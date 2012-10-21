760272b57c96fc41c34c985a3dab14e55eb83f64
Tony Day <zygomega@gmail.com>
2012-10-21 21:27:36 +1100

    in the middle of the load-path bug

--- init.el
+++ init.el
@@ -1,7 +1,20 @@
 
+(defun debug-message (&optional debug-name debug-value)
+        (message (concat 
+           "#+begin_debug\n"
+           (format "%s\n" buffer-file-name)
+           (if debug-name
+             (format "%s\n" debug-name))
+           (if debug-value
+               (format "%s\n" debug-value))
+           "#+end_debug\n")))
+
+(debug-message "load-path init.el entry" load-path)
+(debug-message "user-emacs-directory init.el entry" user-emacs-directory)
+
 ;; see org-init.org for documentation
-(setq working-dir
+(setq user-emacs-directory
       (file-name-directory (or load-file-name (buffer-file-name))))
-(let ((org-dir (expand-file-name "src/org-mode/lisp" working-dir)))
+(let ((org-dir (expand-file-name "src/org-mode/lisp" user-emacs-directory)))
   (add-to-list 'load-path org-dir))
-(org-babel-load-file (expand-file-name "dotemacs.org" working-dir))
+(org-babel-load-file (expand-file-name "dotemacs.org" user-emacs-directory))
