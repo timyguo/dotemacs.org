d053cb2c6be42149ff9196a7e49695491b7d41b0
Tony Day <zygomega@gmail.com>
2012-10-21 11:45:37 +1100

    has broken somehow

--- init.el
+++ init.el
@@ -1,14 +1,7 @@
 
 ;; see org-init.org for documentation
-
-;; remember this directory
 (setq working-dir
       (file-name-directory (or load-file-name (buffer-file-name))))
-
-;; put the org dir on the path if it exists
 (let ((org-dir (expand-file-name "src/org-mode/lisp" working-dir)))
-    ;; add the src directory to the load path
-    (add-to-list 'load-path org-dir))
- 
-;; load up the org-init file
+  (add-to-list 'load-path org-dir))
 (org-babel-load-file (expand-file-name "dotemacs.org" working-dir))
