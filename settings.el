189dbc38c308b16eea85650310089c25f2b0410b
Tony Day <zygomega@gmail.com>
2012-10-22 16:11:33 +1100

    added new snippets

---
+++ settings.el
@@ -0,0 +1,12 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-theme-load-path (quote (custom-theme-directory t "/Users/tonyday/.emacs.d/elpa/zenburn-theme-1.5"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
