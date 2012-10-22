22d39f82b6667f510fd73aa1955ae9a6761e4243
Tony Day <zygomega@gmail.com>
2012-10-22 15:07:53 +1100

    working but not cleaned up

--- init.el
+++ init.el
@@ -1,17 +1,4 @@
 
-(defun debug-message (&optional debug-name debug-value)
-        (message (concat 
-           "#+begin_debug\n"
-           (format "%s\n" buffer-file-name)
-           (if debug-name
-             (format "%s\n" debug-name))
-           (if debug-value
-               (format "%s\n" debug-value))
-           "#+end_debug\n")))
-
-(debug-message "load-path init.el entry" load-path)
-(debug-message "user-emacs-directory init.el entry" user-emacs-directory)
-
 ;; see org-init.org for documentation
 (setq user-emacs-directory
       (file-name-directory (or load-file-name (buffer-file-name))))
