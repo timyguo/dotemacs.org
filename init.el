f24d8c160046f57ad4ee76aecaa15ee54930a7cb
Tony Day <zygomega@gmail.com>
2012-10-28 21:02:17 +1100

    pull back to a minimal startup with just org-mode (norang) and magit.

--- init.el
+++ init.el
@@ -1,9 +1,16 @@
 
-(let ((dotemacs-org-file (quote "/Users/tonyday/.emacs.d/dotemacs.org")))
+(let ((tangled-in (quote "/Users/tonyday/.emacs.d/dotemacs.org")))
 ;; see dotemacs.org for documentation
 (setq user-emacs-directory
-      (file-name-directory (or load-file-name (buffer-file-name))))
-(let ((org-dir (expand-file-name "src/org-mode/lisp" user-emacs-directory)))
-  (add-to-list 'load-path org-dir))
+      (file-name-directory (or load-file-name user-emacs-directory "~/.emacs.d/")))
+(defvar org-dir
+"repos/jwiegley/override/org-mode/lisp"
+"location of the directory containing org-mode")
+(add-to-list 'load-path (expand-file-name org-dir user-emacs-directory))
+(defvar dotemacs-org-file
+"dotemacs.org"
+"Name of the org file containing the main startup code")
+(if (boundp 'tangled-in)
+   (setq dotemacs-org-file tangled-in))
 (org-babel-load-file (expand-file-name dotemacs-org-file user-emacs-directory))
 )
