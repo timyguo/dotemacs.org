050873f5e7fee9e4456fe1c0ba5eeee1fa3d9cac
Tony Day <zygomega@gmail.com>
2012-10-28 12:37:01 +1100

    reduce down orphaned settings and commands. turn on ido, help, dired

--- settings.el
+++ settings.el
@@ -3,13 +3,28 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
  '(custom-theme-load-path (quote (custom-theme-directory t "/Users/tonyday/.emacs.d/elpa/zenburn-theme-1.5")))
- '(org-agenda-files (quote ("~/stuff/emacs/regexp.org" "~/stuff/stuff.org" "~/.emacs.d/README.org" "~/.emacs.d/dotemacs.org" "~/stuff/org/refile.org" "~/stuff/org/bugz.org" "~/stuff/org/snipz.org" "~/stuff/org/org.org" "~/stuff/emacs/emacs.org" "~/stuff/emacs/bindings.org" "~/stuff/sys/sys.org" "~/stuff/biz/scarce.org" "~/stuff/content/life.org" "~/stuff/content/writing.org" "~/stuff/dev/webdev.org" "~/stuff/dev/sandpit.org" "~/stuff/dev/jsdev.org" "~/stuff/factor/factor.org" "~/stuff/content/ideas.org" "~/stuff/factor/parity.org" "~/stuff/quant/rdev.org" "~/stuff/quant/volatility.org" "~/stuff/quant/da.org" "~/stuff/quant/emfx.org" "~/stuff/sys/git.org" "~/git/scarce/scarce.org" "~/git/scarce/mindev.org" "~/git/scarce/sitedev.org")))
+ '(delete-selection-mode t)
+ '(ido-file-extensions-order (quote (".org")))
+ '(indent-tabs-mode t)
+ '(org-agenda-files nil)
+ '(org-confirm-babel-evaluate nil)
  '(org-list-indent-offset 2)
- '(reb-re-syntax (quote string)))
+ '(org-support-shift-select (quote always))
+ '(reb-re-syntax (quote string))
+ '(save-place-file "/Users/tonyday/.emacs.d/places")
+ '(sentence-end-double-space nil)
+ '(shift-select-mode t)
+ '(tool-bar-mode nil)
+ '(transient-mark-mode t)
+ '(truncate-partial-width-windows nil)
+ '(uniquify-buffer-name-style (quote forward) nil (uniquify)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-mode-line-clock ((t (:foreground "red" :box (:line-width -1 :style released-button)))) t)
+ '(trailing-whitespace ((t (:background "disabledControlTextColor"))))
+ '(whitespace-indentation ((t (:background "#3f3f3f" :foreground "#cc9393")))))
