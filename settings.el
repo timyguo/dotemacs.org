8f74ff577f0587c121a54969a98a9425f8a4ac63
Tony Day <zygomega@gmail.com>
2012-11-03 13:44:37 +1100

    added auctex setup from jwiegley

--- settings.el
+++ settings.el
@@ -11,8 +11,10 @@
  '(indent-tabs-mode t)
  '(initial-scratch-message "")
  '(org-confirm-babel-evaluate nil)
+ '(org-export-babel-evaluate nil)
  '(org-list-indent-offset 2)
  '(org-support-shift-select t)
+ '(org-todo-keyword-faces (quote (("TODO" :foreground "#6775ac" :weight bold) ("NEXT" :foreground "#bc8878" :weight bold) ("DONE" :foreground "forest green" :weight bold) ("WAITING" :foreground "orange" :weight bold) ("HOLD" :foreground "magenta" :weight bold) ("CANCELLED" :foreground "forest green" :weight bold) ("PHONE" :foreground "forest green" :weight bold))))
  '(reb-re-syntax (quote string))
  '(save-place-file "/Users/tonyday/.emacs.d/places")
  '(sentence-end-double-space nil)
@@ -27,7 +29,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(diff-added ((((background dark)) (:foreground "#FFFF9B9BFFFF")) (t (:foreground "DarkGreen"))))
+ '(diff-changed ((((background dark)) (:foreground "Yellow")) (t (:foreground "MediumBlue"))))
+ '(diff-context ((((background dark)) (:foreground "White")) (t (:foreground "Black"))))
+ '(diff-file-header ((((background dark)) (:foreground "Cyan" :background "Black")) (t (:foreground "Red" :background "White"))))
+ '(diff-header ((((background dark)) (:foreground "Cyan")) (t (:foreground "Red"))))
+ '(diff-index ((((background dark)) (:foreground "Magenta")) (t (:foreground "Green"))))
+ '(diff-nonexistent ((((background dark)) (:foreground "#FFFFFFFF7474")) (t (:foreground "DarkBlue"))))
  '(org-column ((t (:background "#505050" :strike-through nil :underline nil :slant normal :weight normal :height 120 :family "Monaco"))))
- '(org-mode-line-clock ((t (:foreground "red" :box (:line-width -1 :style released-button)))) t)
+ '(org-mode-line-clock ((t (:foreground "selectedKnobColor" :box (:line-width -1 :style released-button)))) t)
+ '(org-mode-line-clock-overrun ((t (:inherit modeline :foreground "#5650d5" :background "#dccce9"))) t)
  '(trailing-whitespace ((t (:background "disabledControlTextColor"))))
- '(whitespace-indentation ((t (:background "#3f3f3f" :foreground "#cc9393"))) t))
+ '(whitespace-indentation ((t (:background "#3f3f3f" :foreground "#cc9393")))))
